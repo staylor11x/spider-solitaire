32f63c1ad8a6c5fc980cc51c63e4a8e80dd72704
Scott Taylor <Scott2013taylor@gmail.com>
2025-12-22 14:42:07 +0000

    Added CLI printer to verify DTO layer

--- cmd/game/main.go
+++ cmd/game/main.go
@@ -1,2 +1,26 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/staylor11x/spider-solitaire/internal/game"
+	"github.com/staylor11x/spider-solitaire/internal/printer"
+)
+
+func main() {
+	ascii := flag.Bool("ascii", false, "use ASCII suits (S/H/D/C) instead of Unicode")
+	flag.Parse()
+
+	g, err := game.DealInitialGame()
+	if err != nil {
+		log.Fatalf("deal failed: %v", err)
+	}
+
+	view := g.View()
+	out := printer.Render(view, printer.Options{
+		UnicodeSuits: !*ascii,
+	})
+	fmt.Print(out)
+}
