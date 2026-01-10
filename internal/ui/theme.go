dbe458097c2e81124a3574fc19bc2b2f7f3dcbdc
Scott Taylor <Scott2013taylor@gmail.com>
2026-01-10 20:32:32 +0000

    added central place for config

---
+++ internal/ui/theme.go
@@ -0,0 +1,63 @@
+package ui
+
+import "image/color"
+
+type Layout struct {
+	CardWidth            int
+	CardHeight           int
+	PileSpacing          int
+	CardStackGap         int
+	TableauStartX        int
+	TableauStartY        int
+	StatsX               int
+	StatsY               int
+	LogicalWidth         int
+	LogicalHeight        int
+	ErrorDisplayDuration int
+}
+
+type Colors struct {
+	Background        color.RGBA
+	CardFaceUp        color.RGBA
+	CardFaceDown      color.RGBA
+	CardTextBlack     color.RGBA
+	CardTextRed       color.RGBA
+	SelectionOverlay  color.RGBA
+	ErrorPillBG       color.RGBA
+	ErrorPillText     color.RGBA
+	HelpOverlayText   color.RGBA
+	PlaceholderFill   color.RGBA
+	PlaceholderStroke color.RGBA
+	HoverOverlay      color.RGBA
+}
+
+var DefaultTheme = Theme{
+	Layout: Layout{
+		CardWidth:            80,
+        CardHeight:           120,
+        PileSpacing:          100,
+        CardStackGap:         30,
+        TableauStartX:        50,
+        TableauStartY:        150,
+        StatsX:               20,
+        StatsY:               20,
+        LogicalWidth:         1280,
+        LogicalHeight:        720,
+        ErrorDisplayDuration: 180, // 3 seconds at 60 FPS
+	},
+	Colors: Colors {
+		Background:       color.RGBA{R: 0, G: 100, B: 0, A: 255},
+        CardFaceUp:       color.RGBA{R: 255, G: 255, B: 255, A: 255},
+        CardFaceDown:     color.RGBA{R: 0, G: 0, B: 139, A: 255},
+        CardTextBlack:    color.RGBA{R: 0, G: 0, B: 0, A: 255},
+        CardTextRed:      color.RGBA{R: 200, G: 0, B: 0, A: 255},
+        SelectionOverlay: color.RGBA{R: 255, G: 215, B: 0, A: 100},
+        ErrorPillBG:      color.RGBA{R: 200, G: 0, B: 0, A: 220},
+        ErrorPillText:    color.RGBA{R: 255, G: 255, B: 255, A: 255},
+        HelpOverlayBG:    color.RGBA{R: 0, G: 0, B: 0, A: 200},
+        HelpOverlayText:  color.RGBA{R: 255, G: 255, B: 255, A: 255},
+        PlaceholderFill:  color.RGBA{R: 255, G: 255, B: 255, A: 20},
+        PlaceholderStroke:color.RGBA{R: 255, G: 255, B: 255, A: 50},
+        HoverOverlay:     color.RGBA{R: 255, G: 255, B: 255, A: 30},
+	}
+}
